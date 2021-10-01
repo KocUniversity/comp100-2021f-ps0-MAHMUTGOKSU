fee09529c47330671bf5b59684851b45c030f97c
MAHMUTGOKSU <91607951+MAHMUTGOKSU@users.noreply.github.com>
2021-10-01 14:04:08 +0000

    done.

---
+++ main.py
@@ -0,0 +1,7 @@
+number_x = float(input("Type in a number x: "))
+number_y = float(input("Type in a number y: "))
+print ("x**y =", number_x**number_y )
+import numpy as np
+z = np.log2(number_x)
+print("log(x)=",z)
+print("79520")
